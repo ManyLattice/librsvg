ea6c84fac809f7cc3319d15fba4d01494a7935db
Mikhail <manylattice7742@yandex.kz>
2021-05-12 12:47:01 +0600

    Test

---
+++ Sources/librsvg/include/librsvg.c
@@ -0,0 +1,8 @@
+//
+//  librsvg.c
+//  
+//
+//  Created by Михаил Гудзикевич on 12.05.2021.
+//
+
+#include "librsvg.h"
---
+++ Sources/librsvg/include/librsvg.h
@@ -0,0 +1,18 @@
+//
+//  File.h
+//  
+//
+//  Created by Михаил Гудзикевич on 12.05.2021.
+//
+
+#ifndef librsvg_h
+#define librsvg_h
+
+#include <stdio.h>
+
+#endif /* librsvg_h */
+
+struct librsvg {
+    int x;
+    int y;
+};
