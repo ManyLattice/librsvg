877ba99481600b4929989c4eeddf223bd844c163
Mikhail <manylattice7742@yandex.kz>
2021-05-12 10:52:23 +0600

    Make SPM

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "librsvg",
+    products: [
+        .library(
+            name: "librsvg",
+            targets: ["librsvg"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "librsvg",
+            dependencies: [],
+            path: "Sources",
+            resources: [
+                .copy("lib"),
+                .copy("headers")
+            ]),
+        .testTarget(
+            name: "librsvgTests",
+            dependencies: ["librsvg"]),
+    ]
+)
